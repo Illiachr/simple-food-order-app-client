3207899bd446e1ec0e0db98b7871865baeb976b6
Illia Cherkasov <ilja.cherkasov@gmail.com>
2023-08-23 00:22:04 +0300

    implemented Meals component to display available and ordered meals
    added mock meals data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import Layout from "./views/Layout";
 import mealsImg from './assets/meals.jpg';
+import { useEffect, useState } from "react";
+import { TMeals } from "./types/mealsTypes";
+
+const MEALS_MOCK_URL = './src/store/mock/mock-data.json';
 
 function App() {
+  const [meals, setMeals] = useState<TMeals>([]);
+
+  const getMeals = async(url: string): Promise<TMeals> => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const meals = await getMeals(MEALS_MOCK_URL);
+      console.log(meals);
+      setMeals(meals);
+    })();
+  }, []);
+
   const cartClickHandler = () => {
   };
 
@@ -11,6 +31,7 @@ function App() {
       headerBackgroundImg={mealsImg}
       itemsTotal={3}
       onCartButtonClick={cartClickHandler}
+      mealsData={meals}
     />
   )
 }
